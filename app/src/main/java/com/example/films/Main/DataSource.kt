c8935583a98b1fb2ee004f6033e1dbbab0041cd6
Theofil Bodnar <theofil_bodnar@rrsoftware.hu>
2020-09-02 14:01:29 +0200

    1-feladat/Movie data done

---
+++ app/src/main/java/com/example/films/Main/DataSource.kt
@@ -0,0 +1,71 @@
+package com.example.films.Main
+
+import com.example.films.R
+
+class DataSource {
+
+    companion object {
+        fun createDataSet(): ArrayList<Movie> {
+            val list = ArrayList<Movie>()
+
+            list.add(
+                Movie(
+                    "Dolittle!",
+                    2020,
+                    56,
+                    "Many animals and Robert.",
+                    image = R.drawable.dolittle
+                )
+            )
+            list.add(
+                Movie(
+                    "Solo: A Star Wars story",
+                    2018,
+                    69,
+                    "This film present Han great adventures.",
+                    image = R.drawable.solo
+                )
+            )
+
+            list.add(
+                Movie(
+                    "Captain Marvel",
+                    2019,
+                    69,
+                    "Every feminist favourite film.",
+                    image = R.drawable.captain
+                )
+            )
+            list.add(
+                Movie(
+                    "How to Train Your Dragon 3",
+                    2019,
+                    75,
+                    "Animated ",
+                    image = R.drawable.how
+                )
+            )
+            list.add(
+                Movie(
+                    "Pirates of Caribbean",
+                    2003,
+                    80,
+                    "Many arrr and swords.",
+                    image = R.drawable.pirates
+
+                )
+            )
+            list.add(
+                Movie(
+                    "Deadpool",
+                    2016,
+                    80,
+                    "The man life who can not die.",
+                    image = R.drawable.deadpool
+
+                )
+            )
+            return list
+        }
+    }
+}
---
+++ app/src/main/java/com/example/films/Main/Movie.kt
@@ -0,0 +1,9 @@
+package com.example.films.Main
+
+data class Movie(
+    var title: String,
+    var year: Int,
+    var rating: Int,
+    var details: String,
+    var image: Int
+){}
