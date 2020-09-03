e034d7cc975d3fc6ad53f3d6e01a601177c3c104
Theofil Bodnar <theofil_bodnar@rrsoftware.hu>
2020-09-03 11:58:30 +0200

    3-feladat/Hamburger see it and list options

--- app/src/main/java/com/example/films/model/MainActivity.kt
+++ app/src/main/java/com/example/films/model/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.films.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Adapter
 import android.widget.LinearLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.films.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,13 +14,38 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var movieAdapter: MovieRecyclerAdapter
 
+    //menu
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.Rating -> TODO()
+                R.id.Realise -> TODO()
+            }
+            true
+        }
+
         initRecyclerView()
         addDataSet()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun addDataSet(){
         val data = DataSource.createDataSet()
         movieAdapter.submitList(data)
