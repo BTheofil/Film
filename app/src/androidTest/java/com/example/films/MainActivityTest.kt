73f0c3e5e0f37e35c8560f33d3bcc770a0821bb4
Theofil Bodnar <theofil_bodnar@rrsoftware.hu>
2020-09-14 16:49:48 +0200

    6-feladat/preparing testing

--- app/src/androidTest/java/com/example/films/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/films/MainActivityTest.kt
@@ -3,21 +3,31 @@ package com.example.films
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.films.view.MainActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
+    @get:Rule
+    val activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_isListFragmentVisible() {
+        onView(withId(R.id.recycler_view)).check(matches(isDisplayed()))
+    }
+
     @Test
     fun test_writeToCharAndClick() {
 
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
         onView(withId(R.id.search)).perform(click())
-        //onView(withId(R.id.))
+
     }
 }
